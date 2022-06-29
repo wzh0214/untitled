c535e0969584f52047b11ecd6c7557629d684376
wzh0214 <92380850+wzh0214@users.noreply.github.com>
2022-06-29 18:40:00 +0800

    test commit

--- src/main/java/com/wzh/GitTest.java
+++ src/main/java/com/wzh/GitTest.java
@@ -11,6 +11,7 @@ public class GitTest {
         System.out.println("hello.hotfix");
         System.out.println("hot-fix test");
         System.out.println("push-test");
+        System.out.println("pull-test");
 
     }
 
