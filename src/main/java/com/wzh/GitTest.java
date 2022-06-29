9a9dd9d00bb6abd2c3969b85474745eb655b23d8
wzh <wzh@qq.com>
2022-06-29 18:21:50 +0800

    push commit

--- src/main/java/com/wzh/GitTest.java
+++ src/main/java/com/wzh/GitTest.java
@@ -10,6 +10,8 @@ public class GitTest {
         System.out.println("hello.git");
         System.out.println("hello.hotfix");
         System.out.println("hot-fix test");
+        System.out.println("push-test");
+
     }
 
 }
