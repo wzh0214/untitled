3a3ed40b04d42d11a36e389342451b090ca7124c
wzh <wzh@qq.com>
2022-06-29 17:23:20 +0800

    first commit

---
+++ src/main/java/com/wzh/GitTest.java
@@ -0,0 +1,12 @@
+package com.wzh;
+
+/**
+ * @author wang
+ * @date 2022/6/29 - 17:14
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello.git");
+    }
+
+}
