bb7730b7cd14165810d2a969f785872213efa978
wzh <wzh@qq.com>
2022-06-29 17:54:28 +0800

    hot-fix commit2

--- src/main/java/com/wzh/GitTest.java
+++ src/main/java/com/wzh/GitTest.java
@@ -9,6 +9,7 @@ public class GitTest {
         System.out.println("hello.git");
         System.out.println("hello.git");
         System.out.println("hello.hotfix");
+        System.out.println("hot-fix test");
     }
 
 }
